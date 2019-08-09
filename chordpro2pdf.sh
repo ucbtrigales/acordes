159df88cad46ab003ffe1e93df4140098ba66d56
Christian González <christian.gonzalez@lazos.cl>
2019-08-09 17:57:01 -0400

    Nueva canciones y correcciones

--- chordpro2pdf.sh
+++ chordpro2pdf.sh
@@ -4,6 +4,7 @@ IFS=$'\n'
 for i in $(ls Chordpro); do
     name=$(basename "$i" .chordpro)
     if [ ! -f PDF/$name.pdf ]; then
+        echo "$name.chordpro --> $name.pdf"
         chordpro --no-chord-grids -o PDF/$name.pdf Chordpro/$name.chordpro
     fi
 done
